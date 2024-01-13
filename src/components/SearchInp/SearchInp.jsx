e6b1a6481f787bb14ec2dc5f9d921a8c2ed42359
Abdusamad Latipov <abdusamadlatipov67@gmail.com>
2024-01-13 21:26:30 +0300

    input

--- src/components/SearchInp/SearchInp.jsx
+++ src/components/SearchInp/SearchInp.jsx
@@ -2,6 +2,13 @@ import { useState } from 'react'
 import './SearchInp.scss'
 export default function SearchInp() {
     let [value, setValue] = useState('')
+    let [focus, setFocus] = useState(false)
+    const changeFocus = () => {
+        setFocus(false)
+    }
+    const changeBlur = () => {
+        setFocus(true)
+    }
     return(
         <div className="search_content">
             <form action="/">
@@ -10,6 +17,9 @@ export default function SearchInp() {
                 placeholder="Restoran yoki taom nomini kiriting" 
                 onChange={(e)=> setValue(e.target.value)} 
                 value={value}
+                className={focus}
+                onBlur={()=> changeFocus()}
+                onFocus={() => changeBlur()}
                 />
             </form>
         </div>
