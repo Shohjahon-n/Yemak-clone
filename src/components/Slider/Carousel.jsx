436e7aa9bc285c3a4b9df6e0eef6ed1e60d261bb
Sarvarbek Sadullayev <=>
2024-01-14 20:01:09 +0500

    changed slider style

--- src/components/Slider/Carousel.jsx
+++ src/components/Slider/Carousel.jsx
@@ -1,20 +1,43 @@
-import "./Slider.scss"
-import Pitssa from "../../assets/icons/pitssa-icon.png"
-import FastFood from '../../assets/icons/fast-food-icon.png'
-import milliyFood from '../../assets/icons/milliy-food-icon.png'
-import muzqaymoqIcon from '../../assets/icons/muzqaymoq-icon.png'
-import shirinliklar from '../../assets/icons/shirinliklar-icon.png'
-import yevropaFood from '../../assets/icons/yevropa-food-icon.png'
+import React from 'react';
+import './Carousel.scss';
+import Pitssa from '../../assets/icons/pitssa-icon.png';
+import fastFood from '../../assets/icons/fast-food-icon.png';
+import muzqaymoq from '../../assets/icons/muzqaymoq-icon.png';
+import shirinliklar from '../../assets/icons/shirinliklar-icon.png';
+import yevropa from '../../assets/icons/yevropa-food-icon.png';
+import milliy from '../../assets/icons/milliy-food-icon.png';
 
 function Carousel() {
-        return (
-        <div className="slider container">
-            <div className="item">
-                <img src={Pitssa} alt="Pitssa" />
+
+    return (
+        <div className="carousel container">
+            <div className="carousel_item">
+                <img src={Pitssa} alt="Pissa" />
                 <p>Pitssa</p>
+            </div>
+            <div className="carousel_item">
+                <img src={fastFood} alt="fastFood" />
+                <p>Fast Food</p>
+            </div>
+            <div className="carousel_item">
+                <img src={milliy} alt="milliy" />
+                <p>Milliy</p>
+            </div>
+            <div className="carousel_item">
+                <img src={shirinliklar} alt="Shirinliklar" />
+                <p>Shirinliklar</p>
+            </div>
+            <div className="carousel_item">
+                <img src={yevropa} alt="yevropa" />
+                <p>Yevropa</p>
+            </div>
+            <div className="carousel_item">
+                <img src={muzqaymoq} alt="muzqaymoq" />
+                <p>Muzqaymoq</p>
             </div>
         </div>
     );
 }
+
 
 export default Carousel;
