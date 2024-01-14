df96293eca59395352e53b519d0326956e4483df
Shohjahon-n <pazvat56@gmail.com>
2024-01-14 14:11:16 +0500

    login component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import './Header.scss';
 import YemakIcon from '../../assets/icons/yemak-icon.svg';
 import locationImg from '../../assets/icons/location-btn.png';
+import { useState } from 'react';
 //components
 import SearchInp from '../SearchInp/SearchInp';
-
+import Login from '../Login/Login';
 export default function Header() {
+    let [active, setActive] = useState(false)
+
+    const changeActive = () => {
+        setActive(!active)
+    }
+
     return (
         <header>
             <div className="header-content container">
@@ -17,11 +24,14 @@ export default function Header() {
                             <img src={locationImg} alt="Location image" />
                         </div>
                         <div className="log-up">
-                            <button className="login-btn">Kirish</button>
+                            <button onClick={changeActive} className="login-btn">Kirish</button>
                         </div>
                     </div>
                 </div>
                 <SearchInp />
+                {active ?
+                    <Login changeActive={changeActive} />
+                    : null}
             </div>
         </header>
     )
