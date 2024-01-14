52330dd9a8064ef4c54dd06cb74ca11ed3242651
Shohjahon-n <pazvat56@gmail.com>
2024-01-14 19:46:08 +0500

    validate input

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,12 +3,6 @@ import './Login.scss'
 import { useState } from 'react'
 export default function Login({ changeActive }) {
     let [value, setValue] = useState('')
-
-    const changeValue = (e) => {
-        // e.preventDefault()
-        setValue(e.target.value)
-    }
-
     return (
         <div className="login" onClick={changeActive}>
             <div className="login-content" onClick={(e) => e.stopPropagation()}>
@@ -19,10 +13,16 @@ export default function Login({ changeActive }) {
                         alt="close btn" /></div>
                 </div>
                 <hr />
-                <form action="/" onSubmit={(e) => e.preventDefault()}>
-                    <input type="text" placeholder="+998 (__) ___-__-__ "
-                        value={value} onChange={changeValue} />
-                    <button className="login-btn">Tasdiqlash</button>
+                <form action="/">
+                    <input type="number" placeholder="+998 (__) ___-__-__ "
+                        value={value} onChange={(e) => setValue(e.target.value)} />
+                    {value.length !== 12 || !value.startsWith(+998)
+                        ? <button className="login-btn" onClick={() => alert('error')}>Tasdiqlash</button> :
+                        <button className="login-btn active-btn"
+                            onClick={() => {
+                                changeActive(), console.log('User phone:', value)
+                                alert('Tasdiqlandi')
+                            }}>Tasdiqlash</button>}
                 </form>
             </div>
         </div>
